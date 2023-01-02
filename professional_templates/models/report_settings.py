33e97e0091f93ce1b63526423c6cffe284d90c61
Business Solutions For Africa <vincent@biz-4-africa.com>
2023-01-02 20:58:32 +0200

    Fix build account.invoice become account.move

--- professional_templates/models/report_settings.py
+++ professional_templates/models/report_settings.py
@@ -190,47 +190,40 @@ class TemplateSettings(models.Model):
                 if non is uploaded and footer logo is enabled then the company logo will be used")
     odd = fields.Char(
         'Odd parity Color',
-        size=7,
         required=True,
         default="#F2F2F2",
         help="The background color for Odd invoice lines in the invoice")
     even = fields.Char(
         'Even parity Color',
-        size=7,
         required=True,
         default="#FFFFFF",
         help="The background color for Even invoice lines in the invoice")
     theme_color = fields.Char(
         'Theme Color',
-        size=7,
         required=True,
         default="#F07C4D",
         help="The Main Theme color of the invoice. Normally this\
                      should be one of your official company colors")
     text_color = fields.Char(
         'Text Color',
-        size=7,
         required=True,
         default="#6B6C6C",
         help="The Text color of the invoice. Normally this should be one of your official \
                 company colors or default HTML text color")
     name_color = fields.Char(
         'Company Name Color',
-        size=7,
         required=True,
         default="#F07C4D",
         help="The Text color of the Company Name. Normally this should be one of your official\
                 company colors or default HTML text color")
     cust_color = fields.Char(
         'Customer Name Color',
-        size=7,
         required=True,
         default="#F07C4D",
         help="The Text color of the Customer Name. Normally this should be one of your official\
                 company colors or default HTML text color")
     theme_txt_color = fields.Char(
         'Theme Text Color',
-        size=7,
         required=True,
         default="#FFFFFF",
         help="The Text color of the areas bearing the theme color. Normally this should NOT\
@@ -309,7 +302,6 @@ watermark = doc.name
         help="Text to start at 'y' distance from top margin of the PDF body")
     wm_color = fields.Char(
         'Watermark color (Hex)',
-        size=7,
         default="#5A5B5C",
         required=True,
         help="The watermark text color")
